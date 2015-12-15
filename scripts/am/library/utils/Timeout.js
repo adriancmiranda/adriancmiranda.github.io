ce442eb83f837e1bfd40cab0b4f885ab3f1e8847
Adrian Miranda <adrian.miranda@isobar.com.br>
2015-12-15 02:10:49 -0200

    Função timeout

---
+++ scripts/am/library/utils/Timeout.js
@@ -0,0 +1,64 @@
+define([
+	'./Type',
+	'./Class',
+	'./Timer'
+], function(Type, Class, Timer){
+	'use strict';
+
+	var Timeout = new Class(function Timeout(method, delay, groupName){
+		this.method = method;
+		this.params = Array.prototype.slice.call(arguments, 3);
+		this.groupName = Timeout.createGroup(groupName);
+		this.timer = new Timer(delay * 1000, 1);
+		this.timer.on(Timer.COMPLETE, this.onTimerComplete, this);
+		this.timer.start();
+	});
+
+	Timeout.static('createGroup', function(groupName){
+		this.group = this.group || [];
+		if(!this.group[groupName]){
+			this.group[groupName] = [];
+		}
+		this.group[groupName].push(this);
+		return groupName;
+	});
+	
+	Timeout.static('getGroup', function(groupName){
+		return this.group[groupName] || [];
+	});
+	
+	Timeout.static('cancelGroup', function(groupName){
+		var group = this.getGroup(groupName);
+		var id = group.length;
+		while(id--){
+			var timeout = group[id];
+			if(timeout && Type.isFunction(timeout.cancel)){
+				timeout.cancel();
+			}
+		}
+	});
+	
+	Timeout.method('onTimerComplete', function(evt){
+		this.timer.off(Timer.COMPLETE, this.onTimerComplete);
+		if(this.params.length) this.method(this.params);
+		else this.method();
+		this.destroy();
+	});
+
+	Timeout.method('cancel', function(){
+		if(!this.timer)return void 0;
+		this.timer.stop();
+		this.timer.off(Timer.COMPLETE, this.onTimerComplete);
+		this.destroy();
+	});
+
+	Timeout.method('destroy', function(groupName){
+		var group = Timeout.getGroup(groupName);
+		group.splice(group.indexOf(this), 1);
+		this.method = undefined;
+		this.params = undefined;
+		this.timer = undefined;
+	});
+
+	return Timeout;
+});
