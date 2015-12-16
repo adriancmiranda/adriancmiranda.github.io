5a27756b5c3cf1797c4866809e7c124fae5be042
Adrian Miranda <adrian.miranda@isobar.com.br>
2015-12-16 10:28:55 -0200

    Update: Point (seems now to AS3 Point Class)

--- scripts/am/library/geom/Point.js
+++ scripts/am/library/geom/Point.js
@@ -7,39 +7,80 @@ define([
 	var Point = new Class(function Point(x, y){
 		this.setTo(x, y);
 	});
-	
-	Point.static('fromAngle', function(angle, magnitude){
-		return new Point(magnitude * Math.cos(angle), magnitude * Math.sin(angle));
+
+	Point.static('polar', function(length, angle){
+		length = Type.toFloat(length);
+		angle = Type.toFloat(angle);
+		return new Point(length * Math.cos(angle), length * Math.sin(angle));
+	});
+
+	Point.static('distance', function(pointA, pointB){
+		return Math.sqrt(Math.pow(pointA.x - pointB.x, 2) + Math.pow(pointA.y - pointB.y, 2));
+	});
+
+	Point.static('interpolate', function(pointA, pointB, f){
+		return pointA.interpolate(pointB, f);
 	});
 
 	Point.method('setTo', function(x, y){
 		this.x = Type.toFloat(x);
-		this.y = Type.isDefined(y) ? Type.toFloat(y) : this.x;
+		this.y = Type.isDefined(y)? Type.toFloat(y):this.x;
 	});
 
-	Point.method('add', function(p){
-		this.x += p.x;
-		this.y += p.y;
-	});
-
-	Point.method('getMagnitude', function(){
+	Point.method('length', function(){
 		return Math.sqrt(this.x * this.x + this.y * this.y);
 	});
 
-	Point.method('getAngle', function(p){
-		return Math.atan2(this.y, this.x);
+	Point.method('add', function(point){
+		return new Point(this.x + point.x, this.y + point.y);
 	});
 
-	Point.method('equals', function(p){
-		return(p.x === this.x && p.y === this.y);
+	Point.method('subtract', function(point){
+		return new Point(this.x - point.x, this.y - point.y);
 	});
 
-	Point.method('copy', function(p){
-		this.setTo(p.x, p.y);
+	Point.method('offset', function(dx, dy){
+		dx = Type.toFloat(dx);
+		dy = Type.toFloat(dy);
+		this.setTo(this.x + dx, this.x + dy);
+	});
+
+	Point.method('manhattan', function(point){
+		return Math.abs(this.x - point.x) + Math.abs(this.y - point.y);
+	});
+
+	Point.method('normalize', function(thickness){
+		var magnitude = this.length();
+		thickness = Type.isNumeric(thickness) ? Type.toFloat(thickness) : 1;
+		this.x = this.x / magnitude * thickness;
+		this.y = this.y / magnitude * thickness;
+	});
+
+	Point.method('equals', function(toCompare){
+		return(toCompare.x === this.x && toCompare.y === this.y);
+	});
+
+	Point.method('copyFrom', function(sourcePoint){
+		this.setTo(sourcePoint.x, sourcePoint.y);
 	});
 
 	Point.method('clone', function(){
 		return new Point(this.x, this.y);
+	});
+
+	Point.method('toRadians', function(dx, dy){
+		dx = this.x - Type.toFloat(dx);
+		dy = this.y - Type.toFloat(dy);
+		return Math.atan2(dx, dy);
+	});
+
+	Point.method('toDegrees', function(dx, dy){
+		var angle = this.toRadians(dx, dy);
+		return angle * (180 / Math.PI);
+	});
+
+	Point.method('toString', function(){
+		return '(x='+ this.x +', y='+ this.y +')';
 	});
 	
 	return Point;
