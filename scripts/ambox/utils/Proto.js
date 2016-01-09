99df8d483537122205674fa4df1762a0033fb308
Adrian Miranda <adrian.miranda@isobar.com.br>
2016-01-09 13:30:09 -0200

    Update: Proto

--- scripts/ambox/utils/Proto.js
+++ scripts/ambox/utils/Proto.js
@@ -4,33 +4,12 @@
 
 	// Proto (Ecma5)
 	// @role An extensible program-code-template for creating objects
-	// @support IE10+, FF4-20+, SF5.1.4+, CH5+, OP11.60-OP12+, iOS7/8, Android4.4+
+	// @support IE9+, FF4-20+, SF5.1.4+, CH5+, OP11.60-OP12+, iOS7/8, Android4.4+
 	// @author Adrian C. Miranda <adriancmiranda@gmail.com>
-
-	// Object.defineProperties
-	// IE9+, FF4-20+, Webkit, SF5+, CH5+, OP11.60-OP12+, Konq4.13+, Android4.0+, iOS7/8
-	// @see http://kangax.github.io/compat-table/es5/#test-Object.defineProperties
-
-	// Object.defineProperty
-	// IE8[1], IE9+, FF4-20+, SF5[2], SF5.1.4+, CH5+, OP11.60-OP12+, Konq4.13+, Android4.0+, iOS7/8
-	// [1] In Internet Explorer 8 Object.defineProperty only accepts DOM objects
-	// [2] In some versions of Safari 5, Object.defineProperty does not work with DOM objects.
-	// @see http://kangax.github.io/compat-table/es5/#test-Object.defineProperty
-
-	// Function.prototype.bind
-	// IE9[2], IE10+, Android4.3[2], Android4.4+, OP5.0-8.0[2]
-	// [2] Does not support Strict mode
-	// @see http://caniuse.com/#search=Function.prototype.bind
-
-	// Function.prototype.length
-	// @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/length
-	// @see https://github.com/Fyrd/caniuse/issues/2201
-
 	function Proto(caste){
 		if(this instanceof Proto){
-			var noop = function Proto(){};
 			var hasCaste = Type.isFunction(caste);
-			caste = hasCaste? caste : noop;
+			caste = hasCaste? caste : function Proto(){};
 			caste.prototype.toString = this.toString;
 			caste.prototype.flush = this.flush;
 			caste.extends = this.extends;
@@ -38,6 +17,7 @@
 			caste.static = this.static;
 			caste.charge = this.charge;
 			caste.outrun = this.outrun;
+			caste.define = this.define;
 			return caste;
 		}
 		return new Proto(caste);
@@ -67,12 +47,20 @@
 		return context;
 	};
 
-	Proto.bind = (Function.prototype.bind || function(fn, context){
+	Proto.bind = function(fn, context){
 		var args = Type.toArray(arguments, 2);
 		return function(){
-			return fn.apply(context, args.concat(Type.toArray(arguments)));
+			var bound = fn.apply(context, args.concat(Type.toArray(arguments)));
+			bound.__originalFn__ = fn.__originalFn__ || fn;
+			return bound;
 		};
-	});
+	};
+
+	Proto.unbind = function(fn){
+		var originalFn = fn.__originalFn__;
+		delete(fn.__originalFn__);
+		return originalFn;
+	};
 
 	Proto.browse = function(fn){
 		return function(){
@@ -80,19 +68,13 @@
 		};
 	};
 
-	Proto.create = (Object.create || function(prototype){
-		function Proto(){}
-		Proto.prototype = prototype;
-		return new Proto();
-	});
-
-	Proto.prototype.extends = function(superProto){
-		var hasSuper = Type.isFunction(superProto);
-		superProto = hasSuper? superProto : Proto;
-		superProto = superProto.prototype;
-		this.prototype = Proto.create(superProto);
+	Proto.create = Object.create;
+	Proto.prototype.extends = function(target, descriptors){
+		target = Type.isFunction(target)? target.prototype : null;
+		target = !target && Type.isObjectLike(target)? target : null;
+		this.prototype = Proto.create(target, descriptors);
 		this.prototype.constructor = this;
-		this.prototype.super = superProto;
+		this.prototype.super = target;
 		return this;
 	};
 
@@ -129,6 +111,16 @@
 				}
 			}
 		}
+		return this;
+	};
+
+	Proto.prototype.define = function(prop, definition){
+		var definitions = Proto.create(null);
+		var nameList = prop.split('|');
+		for(var id = 0, total = nameList.length; id < total; id++){
+			definitions[nameList[id]] = definition;
+		}
+		Object.defineProperties(this.prototype, definitions);
 		return this;
 	};
 
