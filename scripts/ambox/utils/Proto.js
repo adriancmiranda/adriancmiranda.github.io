b354f36bb4c97d19b4d540ee975ddd00dd8da5b6
Adrian Miranda <adrian.miranda@isobar.com.br>
2016-01-09 11:19:02 -0200

    Update: Proto.bind

--- scripts/ambox/utils/Proto.js
+++ scripts/ambox/utils/Proto.js
@@ -2,10 +2,30 @@
 (function(Ambox){
 	var Type = Ambox.namespace('Type');
 
-	// Proto
+	// Proto (Ecma5)
 	// @role An extensible program-code-template for creating objects
-	// @support everywhere
+	// @support IE10+, FF4-20+, SF5.1.4+, CH5+, OP11.60-OP12+, iOS7/8, Android4.4+
 	// @author Adrian C. Miranda <adriancmiranda@gmail.com>
+
+	// Object.defineProperties
+	// IE9+, FF4-20+, Webkit, SF5+, CH5+, OP11.60-OP12+, Konq4.13+, Android4.0+, iOS7/8
+	// @see http://kangax.github.io/compat-table/es5/#test-Object.defineProperties
+
+	// Object.defineProperty
+	// IE8[1], IE9+, FF4-20+, SF5[2], SF5.1.4+, CH5+, OP11.60-OP12+, Konq4.13+, Android4.0+, iOS7/8
+	// [1] In Internet Explorer 8 Object.defineProperty only accepts DOM objects
+	// [2] In some versions of Safari 5, Object.defineProperty does not work with DOM objects.
+	// @see http://kangax.github.io/compat-table/es5/#test-Object.defineProperty
+
+	// Function.prototype.bind
+	// IE9[2], IE10+, Android4.3[2], Android4.4+, OP5.0-8.0[2]
+	// [2] Does not support Strict mode
+	// @see http://caniuse.com/#search=Function.prototype.bind
+
+	// Function.prototype.length
+	// @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/length
+	// @see https://github.com/Fyrd/caniuse/issues/2201
+
 	function Proto(caste){
 		if(this instanceof Proto){
 			var noop = function Proto(){};
@@ -47,12 +67,12 @@
 		return context;
 	};
 
-	Proto.bind = function(fn, context){
+	Proto.bind = (Function.prototype.bind || function(fn, context){
 		var args = Type.toArray(arguments, 2);
 		return function(){
 			return fn.apply(context, args.concat(Type.toArray(arguments)));
 		};
-	};
+	});
 
 	Proto.browse = function(fn){
 		return function(){
