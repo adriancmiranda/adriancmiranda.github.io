7a44733bef4470e2f4bc453426e257a8ef1290e0
Andrey Miranda <M4IN@MBP-de-Andrey.home>
2016-01-15 06:46:35 -0200

    Update: HttpEvent

--- scripts/ambox/http/HttpEvent.js
+++ scripts/ambox/http/HttpEvent.js
@@ -9,41 +9,14 @@
 	// @support IE10+ fallback
 	// @see http://caniuse.com/#search=XMLHttpRequest
 	var HttpEvent = new Proto(function HttpEvent(data, headers, status, statusText, url){
-		this.url = url;
+		this.url = new URL(url);
 		this.data = data;
 		this.headers = headers;
-		this.status = status;
-		this.statusText = statusText;
-	});
-
-	HttpEvent.define('url', {
-		set:function(value){
-			this._url = new URL(value);
-		},
-		get:function(){
-			return this._url;
-		}
-	});
-
-	HttpEvent.define('status', {
-		set:function(value){
-			this._status = Type.toInt(value);
-			this._status = this._status === 1223? 204 : this._status;
-			if(this._status === 0){
-				this._status = this.data? 200 : this.url.protocol === 'file'? 404 : 0;
-			}
-		},
-		get:function(){
-			return this._status;
-		}
-	});
-
-	HttpEvent.define('statusText', {
-		set:function(value){
-			this._statusText = Type.isString(value)? value : '';
-		},
-		get:function(){
-			return this._statusText;
+		this.statusText = Type.isString(statusText)? statusText : '';
+		this.status = Type.toInt(status);
+		this.status = this.status === 1223? 204 : this.status;
+		if(this.status === 0){
+			this.status = this.data? 200 : this.url.protocol === 'file'? 404 : 0;
 		}
 	});
 
@@ -64,10 +37,6 @@
 
 	HttpEvent.public('toArray', function(){
 		return([this.data, this.headers, this.status, this.statusText, this.url]);
-	});
-
-	HttpEvent.public('toObject', function(){
-		return({ data:this.data, headers:this.headers, status:this.status, statusText:this.statusText, url:this.url });
 	});
 
 	HttpEvent.public('toString', function(){
