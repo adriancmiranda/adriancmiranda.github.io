39eefaed1d0c14c3f06ddca1d22ede9fe322bed1
Adrian Miranda <adrian.miranda@isobar.com.br>
2016-01-16 23:13:37 -0200

    Update: patterns

--- scripts/ambox/common/patterns.js
+++ scripts/ambox/common/patterns.js
@@ -10,7 +10,7 @@
 		isTypedArray:/^((Uint8(Clamped)?)|(Uint16)|(Uint32)|(Int8)|(Int16)|(Int32)|(Float(32)|(64))Array)|ArrayBuffer$/,
 		support3d:/^(css|matrix|rotate|scale|translate|transform(s)?)?3d$/,
 		functionDeclaration:/^.*function\s([^\s]*|[^\(]*)\([^\x00]+$/,
-		isXHRMethod:/^(get|post|head|put|delete|options)$/i,
+		isHttpMethod:/^(jsonp|get|delete|head|post|put|patch)$/i,
 		objectWrapper:/^(\[object(\s|\uFEFF|\xA0))|(\])$/g,
 		cssMeasure:/^([+-]?(?:\d+|\d*\.\d+))([a-z]*|%)$/,
 		noValue:/^(Undefined|Null|NaN)$/i,
