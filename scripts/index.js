c29ec0d4c9a4075e2c2e8a7fd135b1c0803290b5
Adrian C. Miranda <adriancmiranda@gmail.com>
2016-07-31 15:25:56 -0300

    Update: package

--- scripts/index.js
+++ scripts/index.js
@@ -3,26 +3,19 @@
 require('trace');
 require('Proto');
 
-var Aplication = new Proto(function Aplication(states){
+var Aplication = new Proto(function(states){
   this.super(states, 'created');
-  this.redditURL = 'http://www.reddit.com/r/perfectloops/top.json?sort=top&t=week';
-  // this.start();
+  this.start();
 })
 
 .extends(require('./ambox/index'))
+
 .public('load', function(url){
-  console.log('Aplication.load(%s)', url);
   this.super(url);
 })
 
 .public('start', function(){
-  console.log('Aplication.start');
   this.super();
-})
-
+});
 
 module.exports = new Aplication(require('./portifa/states'));
-console.log('--');
-console.log('aplication:', module.exports);
-console.log('aplication.teste:', module.exports.load('http://teste.com'));
-console.log('aplication.teste:', module.exports.teste);
