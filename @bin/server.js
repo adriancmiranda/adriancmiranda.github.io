e15a987a04b21df86ae297617651aee5d2141dfd
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-04-08 15:10:14 -0300

    chore: proxy

--- @bin/server.js
+++ @bin/server.js
@@ -4,18 +4,19 @@ const webpack = require('webpack');
 const pipeline = require('./pipeline');
 const config = require('./');
 
-const { NODE_ENV = JSON.parse(config.common.get('dev.env.NODE_ENV')) } = process.env;
 const { NODE_PATH = config.common.res('path.output.bundle') } = process.env;
+const { NODE_ENV = JSON.parse(config.common.get('dev.env.NODE_ENV')) } = process.env;
 const { PORT = config.common.get('dev.port') } = process.env;
 const { HOST = config.common.get('dev.host') } = process.env;
 
+const proxy = config.common.get('dev.proxy');
 const testing = /^testing$/i.test(NODE_ENV);
 const webpackConfig = config({ dev: !testing, run: argv.run });
 const multiCompiler = webpack(webpackConfig);
 const app = express();
 
 app.set('env', NODE_ENV);
-app.use(pipeline(multiCompiler, {}));
+app.use(pipeline(multiCompiler, { log: argv.log, notify: argv.notify, proxy }));
 
 module.exports = app.listen(PORT, HOST, (err) => {
 	console.log(err || 'Wait for it...');
