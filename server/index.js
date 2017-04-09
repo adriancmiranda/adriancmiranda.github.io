042e092cb16426f1c6936bfe8f0c5f6c01038802
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-04-09 00:45:14 -0300

    fix: server call

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ import app from '~/app'; // eslint-disable-line
 console.log('app:', app);
 export default (request, response, next) => {
 	if (request.url === '/api') {
-		response.send('server: up & running', request, response);
+		console.log(request, response);
+		response.send('server: up & running');
 	} else {
 		next();
 	}
