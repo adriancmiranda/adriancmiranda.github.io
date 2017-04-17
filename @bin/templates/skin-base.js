01f2d92f18aba1dfa22310f49a85d280c4de3b0e
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-04-17 20:11:02 -0300

    chore: style-base config

--- @bin/templates/skin-base.js
+++ @bin/templates/skin-base.js
@@ -1,102 +1,32 @@
-const is = require('is');
 const autoprefixer = require('autoprefixer');
-const fileLoader = require.resolve('../loaders/file-loader');
-const asset = require('./asset');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const { styleLoaders } = require('webpack-cfg/templates');
 
-exports.parseEntry = ($, tmpl, filename) =>
-	tmpl.replace('%s', $('path.entry.style', filename));
-;
+module.exports.postcss = $ => [
+	autoprefixer(Object.assign({}, $('style.autoprefixer'), {
+		browsers: $('pkg.browsers'),
+	})),
+];
 
-exports.parseEntries = ($, tmpl) => {
-	const parse = exports.parseEntry.bind(exports, $, tmpl);
-	const entry = $('style.entry') || [];
-	if (is.string(entry)) {
-		return parse(entry);
-	} else if (is.object(entry)) {
-		entry = Object.keys(entry).map((name) => entry[name]);
-	} if (is.array(entry)) {
-		return entry.map(parse).join(' ');
+module.exports.style = ($, fallbackStyle = 'style-loader') => {
+	const options = styleLoaders($, fallbackStyle, module.exports.postcss);
+	console.log(options.use.scss)
+	if (options.extract) {
+		Object.keys(options.use).map((name) => {
+			const fallback = options.use[name].shift();
+			options.use[name] = ExtractTextPlugin.extract({
+				publicPath: options.publicPath,
+				use: options.use[name],
+				fallback,
+			});
+			return options.use[name];
+		});
 	}
-	throw new TypeError('Invalid style.entry');
+	return options.use;
 };
 
-const cssLoader = ($, publicPath, extract, fallback, prefix = '') => {
-	const action = $('lifecycle');
-	const sourceMap = $(`${action}.sourceMap`);
-	const minimize = $('argv.dev');
-	const cssLoader = {
-		loader: 'css-loader',
-		options: Object.assign({}, $(`style.${prefix}css`), {
-			minimize,
-			sourceMap,
-		}),
-	};
-	if (is.empty(fallback) || extract) {
-		return [{
-			loader: fileLoader,
-			options: asset.resolve($, 'style'),
-		}, {
-			loader: 'extract-loader',
-		}, cssLoader];
-	}
-	return [fallback, cssLoader];
-};
-
-exports.style = ($, fallback = 'style-loader') => {
-	const action = $('lifecycle');
-	const parse = exports.parseEntries;
-	const env = $(`${action}.env.NODE_ENV`);
-	const sourceMap = $(`${action}.sourceMap`);
-	const publicPath = $(`${action}.publicPath`);
-	const extract = $('argv.dev');
-	return {
-		scss: cssLoader($, publicPath, extract, fallback).concat({
-			loader: 'sass-loader',
-			options: Object.assign({}, $('style.scss'), {
-				sourceMap,
-				data: [
-					`$env: ${env || 'nil'};`,
-					`@import "${parse($, '@import "~%s"')}";`
-				].join(' '),
-			}),
-		}),
-		sass: cssLoader($, publicPath, extract, fallback).concat({
-			loader: 'sass-loader',
-			options: Object.assign({}, $('style.sass'), {
-				indentedSyntax: true,
-				sourceMap,
-				data: [
-					`$env: ${env || 'nil'};`,
-					`@import "${parse($, '@import ~%s')}";`
-				].join(' '),
-			}),
-		}),
-		stylus: cssLoader($, publicPath, extract, fallback).concat({
-			loader: 'stylus-loader',
-			options: Object.assign({}, $('style.stylus'), {
-				sourceMap,
-			}),
-		}),
-		styl: cssLoader($, publicPath, extract, fallback).concat({
-			loader: 'stylus-loader',
-			options: Object.assign({}, $('style.styl'), {
-				sourceMap,
-			}),
-		}),
-		less: cssLoader($, publicPath, extract, fallback).concat({
-			loader: 'less-loader',
-			options: Object.assign({}, $('style.less'), {
-				sourceMap,
-			}),
-		}),
-		css: cssLoader($, publicPath, extract, fallback),
-		postcss: cssLoader($, publicPath, extract, fallback, 'post'),
-	};
-};
-
-exports.rules = ($, fallback) => {
-	const use = exports.style($, fallback);
-	console.log('use:\n', use);
+module.exports.rules = ($, fallback) => {
+	const use = module.exports.style($, fallback);
 	return Object.keys(use).map((ext) => ({
 		test: new RegExp(`\\.${ext}$`),
 		use: use[ext],
