029a98e3cd8cc2df7dda3ac9f85fb7002a648c78
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-06-03 16:31:23 -0300

    chore: templates

---
+++ public/components/index.js
@@ -0,0 +1,9 @@
+import Works from './works';
+import About from './about';
+import Contact from './contact';
+
+export default {
+	Works,
+	About,
+	Contact,
+};
---
+++ public/components/works/index.js
@@ -0,0 +1,4 @@
+export default class Works {
+	constructor() {
+	}
+};
