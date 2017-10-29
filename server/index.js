6db0b2fbd0e853a2eb9144a457f9bb33a95df7b9
Adrian Miranda <adriancmiranda@gmail.com>
2017-10-29 20:18:27 -0200

    chore: server config

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 import app from '~/app'; // eslint-disable-line
+import opn from 'opn';
+import pipeline from '../@bin/webpack/pipeline';
 
-console.log('app:', app);
-export default (request, response, next) => {
-	if (request.url === '/api') {
-		console.log(request, response);
-		response.send('server: up & running');
-	} else {
-		next();
+const { NODE_ENV = 'development', HOST = '0.0.0.0', PORT = 3001 } = process.env;
+
+app.set('env', NODE_ENV);
+app.use(pipeline());
+
+module.exports = app.listen(PORT, HOST, (err) => {
+	if (err) {
+		console.error(`Failed start server: ${(err.message || err.toString())}`);
+		process.exit(128);
 	}
-};
+	opn(`http://${HOST}:${PORT}/?debug-nav`);
+});
