e46e69f73605d4c73684aed9340a3177c7a28b38
Adrian Miranda <adriancmiranda@gmail.com>
2017-10-29 20:16:52 -0200

    chore: server config

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
-import express from 'express';
 import path from 'path';
+import express from 'express';
 
 const app = express();
-
 app.enable('trust proxy');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use((request, response, next) => {
+	if (request.url === '/api') {
+		response.send('server: up & running');
+	} else next();
+});
 
 export default app;
