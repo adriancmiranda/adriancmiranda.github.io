bf8bbfb83e871031fd8158aee5170072e414519b
Adrian Miranda <adriancmiranda@gmail.com>
2017-10-29 20:12:23 -0200

    chore: add middleware file

--- @bin/pipeline/index.js
+++ @bin/pipeline/index.js
@@ -1,74 +1,27 @@
-const is = require('is');
-const path = require('path');
-const { Router, static } = require('express');
-const httpProxy = require('http-proxy-middleware');
-const reporter = require('./reporter');
-const node = require('./node');
-const web = require('./web');
-
-/*!
-|* Express middleware for universal application development with webpack.
-|* Based on `webpack-universal-middleware`
-|* @see https://github.com/webpack/docs/wiki/node.js-api
-|* @see https://expressjs.com/en/guide/writing-middleware.html
-|* @see https://expressjs.com/en/guide/using-middleware.html
-|* @see http://expressjs.com/pt-br/api.html#router
- `*/
-module.exports = (multiCompiler, options = {}) => {
-	if (!is.array(multiCompiler.compilers)) {
-		multiCompiler = { compilers: [multiCompiler] };
-	}
-	if (!options.reporter) {
-		options.reporter = reporter(options);
-	}
-	if (is.primitive(options.proxy)) {
-		options.proxy = {};
-	}
-	if (!options.static) {
-		options.static = ['./static'];
-	} else if (!is.array(options.static)) {
-		options.static = [options.static];
-	}
-
-	const app = Router();
-
-	options.static = options.static.filter((item) => {
-		let realPath;
-		let virtualPath;
-		if (item === Object(item) && is.string(item.virtualPath) && is.string(item.realPath)) {
-			realPath = path.resolve(item.realPath);
-			virtualPath = item.virtualPath;
-			app.use(virtualPath, static(realPath));
-		} else if (is.string(item)) {
-			realPath = path.resolve(item);
-			app.use(static(realPath));
-		}
-		return (virtualPath || realPath) && { virtualPath, realPath };
+/* eslint-disable global-require */
+const { NODE_ENV = 'production' } = process.env;
+if (/^(dev(elopment)?|test(ing)?)$/gi.test(NODE_ENV)) {
+	const { argv } = require('yargs');
+	const webpack = require('webpack');
+	const middleware = require('./middleware.js');
+	const config = require('../');
+	const { NODE_PATH = config.common.res('path.output.bundle') } = process.env;
+	const { PORT = config.common.res('dev.port') } = process.env;
+	const { HOST = config.common.res('dev.host') } = process.env;
+	const static = config.common.res('path.output.static');
+	const proxy = config.common.res('dev.proxy');
+	const autoOpenBrowser = config.common.res('dev.autoOpenBrowser');
+	const testing = /^testing$/i.test(NODE_ENV);
+	const webpackConfig = config({ dev: !testing, run: argv.run });
+	const multiCompiler = webpack(webpackConfig);
+	module.exports = () => middleware(multiCompiler, {
+		log: argv.log,
+		notify: argv.notify,
+		static,
+		proxy,
 	});
-
-	Object.keys(options.proxy).forEach((context) => {
-		let target = options.proxy[context];
-		if (is.string(target)) {
-			target = { target };
-		}
-		if (target.virtualPath) {
-			const virtualPath = target.virtualPath;
-			delete target.virtualPath;
-			app.use(virtualPath, httpProxy(target.filter || context, target));
-		} else {
-			app.use(httpProxy(target.filter || context, target));
-		}
-	});
-
-	multiCompiler.compilers.forEach((compiler) => {
-		switch (compiler.options.target) {
-			case 'web': app.use(web(compiler, options)); break;
-			case 'node': app.use(node(compiler, options)); break;
-			default: throw new Error(`Unsupported compiler target "${
-				compiler.options.target
-			}" in config.`);
-		}
-	});
-
-	return app;
+} else module.exports = () => (req, res, next) => {
+	if (request.url === '/unkn') {
+		response.send('Unkn up & running');
+	} else next();
 };
